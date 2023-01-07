c61ba85c97225ca1a41478e5a28ef82cc1ffe524
larry <JLawrenceB1@gmail.com>
2023-01-07 07:14:56 -0800

    Refactored.

--- modules/s3/examples/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_bucket_id" {
-  description = "S3 bucket id"
-  value       = module.s3_basic.s3_bucket_id
-}
--- modules/s3/modules/s3/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "s3_bucket_id" {
-  description = "S3 bucket ID"
-  value = module.s3_bucket.s3_bucket_id
-}
